4c86941f57816d35792c148bad83c5327bc92bb8
Dimitrije212 <sekulic_dimitrije@yahoo.com>
2018-12-25 19:19:52 +0100

    Treci Report

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include<GL/glut.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<math.h>
 
 #define TIMER_INTERVAL 20
 #define TIMER_ID 1
+#define PI 3.14159265358979323
 
 static int window_width, window_height;
 
@@ -14,8 +16,8 @@ static void on_timer(int value);
 
 int moving_object = 0;
 int animation_ongoing = 0;
-float gravity = 0;
-
+int jumping_animation = 0;
+float jumping = 0;
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -45,38 +47,43 @@ static void on_keyboard(unsigned char key, int x, int y)
             break;
         /* Pomeramo objekat udesno i iscrtavamo ga*/
         case 'd':
-            {
-                 moving_object++;
-                 glutPostRedisplay();
-            }
+            moving_object++;
+            glutPostRedisplay();
             break;
         /* Pomeramo objekat ulevo i iscrtavamo ga*/
         case 'a':
-            {
-                 moving_object--;
-                 glutPostRedisplay();
-            }
+            moving_object--;
+            glutPostRedisplay();
             break;
+        case 'w':
+            if(!jumping_animation) {
+                    jumping_animation = !jumping_animation;
+                    glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+                }
+                break;
         /* Proba timera pritiskom na space */
-        case ' ':
+        /*case ' ':
             animation_ongoing = 1 - animation_ongoing;
             glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
-            break;
+            break;*/
     }
 }
 
 static void on_timer(int value)
 {
-    if(value != TIMER_ID)
-    return;
-    gravity += 0.01;
-    
-
-    glutPostRedisplay();
-
-    if(animation_ongoing)
+    if(value == TIMER_ID)
     {
-        glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+        jumping += 0.01;
+        if(jumping >= 1) {
+            jumping_animation = 0;
+            jumping = 0;
+        }
+        glutPostRedisplay();
+        
+        if(jumping_animation)
+            glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+           
+             
     }
 }
 
@@ -100,15 +107,27 @@ static void drawStage()
 {
     glColor3f(0.3, 0.3, 0.3);
     glBegin(GL_POLYGON);
-        glVertex3f(-2, -0.51,   5);
-        glVertex3f( 2, -0.51,   5);
-        glVertex3f( 2, -0.51, -30);
-        glVertex3f(-2, -0.51, -30);
+        glVertex3f(-2, -0.51,   3);
+        glVertex3f( 2, -0.51,   3);
+        glVertex3f( 2, -0.51, -100);
+        glVertex3f(-2, -0.51, -100);
     glEnd();
 }
 
 static void on_display(void)
 {
+    /*Nameštamo osvetljenje*/
+    GLfloat light_position[] = {0, 2, 1.5, 0};
+    GLfloat light_ambient[] = {1, 1, 1, 1};
+    GLfloat light_diffuse[] = {0.8, 0.8, 0.8, 1};        
+    GLfloat light_specular[] = {0.1, 0.1, 0.1, 1};
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glViewport(0, 0, window_width, window_height);
@@ -119,14 +138,15 @@ static void on_display(void)
     glMatrixMode(GL_MODELVIEW);
 
     glLoadIdentity();
-    gluLookAt(0, 2, 5, 0, 0, 0, 0, 1, 0);
+    gluLookAt(0, 1.5, /*4*/5, 0, 0, 0, 0, 1, 0);
     
     /*Iscrtavamo podijum*/
     drawStage();
     
     /*Iscrtavamo objekat*/
     glPushMatrix();
-        glTranslatef(moving_object, -gravity, 0);
+        glTranslatef(moving_object,1 - cos(2*jumping*PI), 0);
+        glRotatef(15*(1 - cos(2*PI*jumping)), 1, 0, 0);
         drawCube();
     glPopMatrix();
 
